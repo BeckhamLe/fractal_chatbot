6c354a6e7cf2e103d44e1780c7749ee7465e34ac
Beckham Le <beckhamle@Beckhams-MacBook-Pro.local>
2026-02-12 18:11:08 -0500

    Refactor data storage from SQLite to Supabase with Drizzle ORM

--- vite-express-project/src/server/storage.ts
+++ vite-express-project/src/server/storage.ts
@@ -4,11 +4,11 @@ export interface Storage {
     // ADDING MESSAGE TO CONVERSATION IN DATA STORAGE SOURCE
     // takes in conversation id and message object as parameters and returns a Conversation object
     // uses id to find the conversation in storage, add new message to array of messages in convo, then return the updated conversation to server
-    addMessageToConversation(convoId: string, message: Message): Conversation
+    addMessageToConversation (convoId: string, message: Message): Promise<Conversation>
 
     // returns the title and conversation id of all conversations currently in storage
-    getConversations(): {convoId: string, convoTitle: string}[]
+    getConversations (): Promise<{convoId: string, convoTitle: string}[]>
 
-    getConversation(convoId: string): Conversation
-    createConversation(): Conversation
+    getConversation (convoId: string): Promise<Conversation>
+    createConversation (): Promise<Conversation>
 }
